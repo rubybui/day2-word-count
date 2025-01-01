3391ef4ab243a1cbea73d74ac940c864116a8b3e
Ruby Bui <ruby@coderpush.com>
2024-12-31 21:58:47 -0800

    add all command

--- ccwc.py
+++ ccwc.py
@@ -11,15 +11,66 @@ def count_bytes(file_path):
         print(f"ccwc: {file_path}: No such file or directory")
         exit(1)
 
+def count_lines(file_path):
+    try:
+        with open(file_path, "r") as file:
+            line_count = sum(1 for line in file)
+        return line_count
+    except FileNotFoundError:
+        print(f"ccwc: {file_path}: No such file or directory")
+        exit(1)
+
+def count_words(file_path):
+    try:
+        with open(file_path, 'r') as file:
+            content = file.read()
+            words = content.split()
+            return len(words)
+    except FileNotFoundError:
+        print(f"File '{file_path}' not found.")
+        return 0
+
+def count_characters(file_path):
+    try:
+        with open(filename, 'r') as file:
+            content = file.read()
+            return len(content)
+    except FileNotFoundError:
+        print(f"File '{file_path}' not found.")
+        return 0
+
 def main():
     parser = argparse.ArgumentParser(description="Count bytes in a file.")
     parser.add_argument("-c", action="store_true", help="Count bytes in the file")
+    parser.add_argument("-l", action="store_true", help="Count the number of lines in a file")
+    parser.add_argument("-w", action="store_true", help="Count the number of words in a file")
+    parser.add_argument("-m", action="store_true", help="Count the number of characters in a file")
     parser.add_argument("file", help="The file to count bytes in")
     args = parser.parse_args()
+    if not (args.c or args.l or args.w or args.m):
+        args.c = True
+        args.l = True
+        args.w = True
+
+    results = []
 
     if args.c:
         byte_count = count_bytes(args.file)
-        print(f"{byte_count:8} {args.file}")
+        results.append(f"{byte_count:8}")
+
+    if args.l:
+        line_count = count_lines(args.file)
+        results.append(f"{line_count:8}")
+
+    if args.w:
+        word_count = count_words(args.file)
+        results.append(f"{word_count:8}")
+
+    if args.m:
+        character_count = count_characters(args.file)
+        results.append(f"{character_count:8}")
+
+    print("".join(results), args.file)
 
 if __name__ == "__main__":
     main()
